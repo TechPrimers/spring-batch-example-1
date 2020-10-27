f724e1eab4c9b64565fc0cfa615ef5e913bfffc0
Bryan Eaton <eatonbm@ornl.gov>
2020-10-27 10:33:53 -0400

    upgraded to spring boot 2.3.4

--- src/main/java/com/techprimers/springbatchexample1/batch/DBWriter.java
+++ src/main/java/com/techprimers/springbatchexample1/batch/DBWriter.java
@@ -11,13 +11,16 @@ import java.util.List;
 @Component
 public class DBWriter implements ItemWriter<User> {
 
-    @Autowired
     private UserRepository userRepository;
 
-    @Override
-    public void write(List<? extends User> users) throws Exception {
+    @Autowired
+    public DBWriter (UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
+    @Override
+    public void write(List<? extends User> users) throws Exception{
         System.out.println("Data Saved for Users: " + users);
-        userRepository.save(users);
+        userRepository.saveAll(users);
     }
 }
